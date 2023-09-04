82e4e96574b302477c7b88a277f390da19f474e1
Shazia Badur <shaziabadur@Shazias-MBP.lan>
2023-09-04 15:03:20 -0400

    testing change for deploy

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import MuiCard from './components/MuiCard';
 
 function App() {
   return (
-    <MuiCard name='Shazia Badur' email='shazbadur@gmail.com' githubURL='github.com/shaziabadur' linkedInURL="linkedin.com/in/shazia-badur"/>
-    
+    <>
+      <MuiCard 
+        name='Shazia Badur' 
+        email='shazbadur@gmail.com' 
+        githubURL='github.com/shaziabadur' 
+        linkedInURL="linkedin.com/in/shazia-badur"/>
+
+      <p>Test deploy</p>    
+    </>
     );
 }
 
